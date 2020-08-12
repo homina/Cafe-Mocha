ab4b3e8870481bf52d8d92ca7ced61c843793dc0
ef <homina@users.noreply.github.com>
2020-08-12 10:21:35 +0700

    Initial Commit

---
+++ Cafe Mocha/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Cafe Mocha
+//
+//  Created by ef on 12/08/20.
+//  Copyright © 2020 ef. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
